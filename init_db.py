c0a4b11df329ab26d15e3d63c008d7a8cfc7dfaf
Hyejin Park <phyejin@naver.com>
2021-01-11 21:17:39 +0900

    doc2에 icon추가

--- init_db.py
+++ init_db.py
@@ -8,10 +8,10 @@ import schedule
 client = MongoClient('localhost', 27017)
 db = client.dbmyproject
 
+
 def get_article():
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-
 
     data = requests.get('https://news.naver.com/main/home.nhn', headers=headers)
 
@@ -27,20 +27,20 @@ def get_article():
             if title:
                 url = article.select_one('a')['href']
 
-            # 기사를 겹치지 않고 가지고 오기 위한 유니크 키 생성.
+                # 기사를 겹치지 않고 가지고 오기 위한 유니크 키 생성.
                 unique_url = f"{url}"
 
                 parts = urlparse(unique_url)
 
                 query_string = parse_qs(parts.query)
-            # print(query_string)
+                # print(query_string)
 
                 sid1 = query_string["sid1"][0]
                 oid = query_string["oid"][0]
                 aid = query_string["aid"][0]
 
                 unique_key = f"{sid1}-{oid}-{aid}"
-            # print(unique_key)
+                # print(unique_key)
 
                 doc = {
                     'unique_key': unique_key,
@@ -48,13 +48,14 @@ def get_article():
                     'url': url,
                 }
 
-            # # unique_key 값이 중복되지 않을때 db에 insert 하기.
-            # Tutor : document가 있는지 찾기
+                # # unique_key 값이 중복되지 않을때 db에 insert 하기.
+                # Tutor : document가 있는지 찾기
                 document = db.articleurl.find_one({"unique_key": unique_key})
 
-            # Tutor : document가 없다면 추가하기
+                # Tutor : document가 없다면 추가하기
                 if document is None:
                     db.articleurl.insert_one(doc)
+
 
 def post_article():
     urls = list(db.articleurl.find())
@@ -88,34 +89,48 @@ def post_article():
 
             unique_key = f"{sid1}-{oid}-{aid}"
 
-            doc2 = {
-                'unique_key': unique_key,
-                'url': url['url'],
-                'title': title,
-                'desc': og_desc,
-                'date': date,
-                'media': media,
-            }
+            # articleurl DB의 데이터 개수를 이용하여 번호를 매긴 후, 딕셔너리로 저장할때 순서대로 번호 매겨주기.
+            # num = db.articleurl.countDocument()
+            # for i in range(1,num):
+            #     print("%s번째 기사입니다." %i)
+            #     return
 
-            # unique_key 값이 중복되지 않을때 db에 insert 하기.
-            # Tutor : document가 있는지 찾기
-            document2 = db.latestNews.find_one({"unique_key": unique_key})
+            # url주소가 네이버뉴스홈이면 korea 아이콘으로 db에 같이 저장하기.
+            if target_url[:27] == "https://news.naver.com/main":
+                doc2 = {
+                    # 'num': num,
+                    'icon': "../static/south-korea.png",
+                    'unique_key': unique_key,
+                    'url': url['url'],
+                    'title': title,
+                    'desc': og_desc,
+                    'date': date,
+                    'media': media,
+                }
 
-            # Tutor : document가 없다면 추가하기
-            if document2 is None:
-                db.latestNews.insert_one(doc2)
+                # unique_key 값이 중복되지 않을때 db에 insert 하기.
+                # Tutor : document가 있는지 찾기
+                document2 = db.latestNews.find_one({"unique_key": unique_key})
 
-        # print(url['url'], title, og_desc, date, media)
+                # Tutor : document가 없다면 추가하기
+                if document2 is None:
+                    db.latestNews.insert_one(doc2)
+
+    # print(url['url'], title, og_desc, date, media)
+
 
 post_article()
+get_article()
 
-def job():
-    get_article()
+# def job():
+#    get_article()
 
-def run():
-    schedule.every(3).minutes.do(job)  # 3분에 한번씩 실행
-    while True:
-        schedule.run_pending()
 
-if __name__ == "__main__":
-    run()
+# def run():
+#    schedule.every(3).minutes.do(job)  # 3분에 한번씩 실행
+#    while True:
+#        schedule.run_pending()
+
+
+# if __name__ == "__main__":
+#    run()
